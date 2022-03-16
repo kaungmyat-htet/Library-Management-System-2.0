f4cf05df3fd38e57509c4b9130c7d3eb74053b3d
Kaung Myat <kaungmyathtet.info@gmail.com>
2022-03-16 13:43:17 +0700

    add book

---
+++ src/com/model/Book.java
@@ -0,0 +1,64 @@
+package com.model;
+
+import javax.security.auth.Subject;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private final String isbn;
+    private String title;
+    private Author[] authors;
+    private BookSubject subject;
+    private String language;
+    private int numberOfPages;
+
+    public Book(String isbn, String title, Author[] authors, BookSubject subject, String language, int numberOfPages) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+        this.subject = subject;
+        this.language = language;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public Book(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public BookSubject getSubject() {
+        return subject;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", authors=" + Arrays.toString(authors) +
+                ", subject=" + subject +
+                ", language='" + language + '\'' +
+                ", numberOfPages=" + numberOfPages +
+                '}';
+    }
+}
